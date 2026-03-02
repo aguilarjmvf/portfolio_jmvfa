db1d6b7ea4c5ae9da372cc10753655b72f598aa2
MisdBSS <business.solution.misd@gmail.com>
2026-03-02 16:47:41 +0800

    Updates

---
+++ precache-manifest.bcf46b537c6f399dadbb8990358a9180.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1513d069492978dea484d88311ca8d0e",
+    "url": "/portfolio_jmvfa/index.html"
+  },
+  {
+    "revision": "f0ced3de6bc4d8d466e2",
+    "url": "/portfolio_jmvfa/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c938c416d9ca461510ce",
+    "url": "/portfolio_jmvfa/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "f0ced3de6bc4d8d466e2",
+    "url": "/portfolio_jmvfa/static/js/2.f7c711d5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio_jmvfa/static/js/2.f7c711d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c938c416d9ca461510ce",
+    "url": "/portfolio_jmvfa/static/js/main.00a29f7a.chunk.js"
+  },
+  {
+    "revision": "9acfd4af29398e91bdd6",
+    "url": "/portfolio_jmvfa/static/js/runtime-main.05ea041f.js"
+  }
+]);
